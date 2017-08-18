9fb2c3d1dd31e61f4ab6e46a0b284afdd040b7e7
ronaldomb98 <ronaldomb98@gmail.com>
2017-08-18 13:24:41 -0500

    correccion de un problema en myblog

---
+++ MyBlog/app/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Product
+ *
+ * @mixin \Eloquent
+ */
+class Product extends Model
+{
+    /* protected $table = 'hobbie'; */
+    protected $fillable = [
+        'name', 'short', 'body', # en caso de no agregar ninguno, lo deja en blanco
+    ];
+}
